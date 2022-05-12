c9cf26ed0e2b2092627d09dcff76f7306cc75a89
Kevin Gilbert Toding <122202002832@mhs.dinus.ac.id>
2022-05-13 00:04:11 +0800

    add slider image of volcanoes :volcano:

--- volcanoes/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$server        = "localhost"; // sesuaikan alamat server anda
-$user        = "root"; // sesuaikan user web server anda
-$password    = ""; // sesuaikan password web server anda
-$database    = "volcano"; // sesuaikan database web server anda
-
-$con = mysqli_connect($server, $user, $password, $database);
